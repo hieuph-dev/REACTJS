40b0eb502114f8cd51400ac57756a9484f2531eb
Hieu Pham <hieupham250201@gmail.com>
2022-10-15 00:15:35 +0700

    upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ import Header from './Header/Header';
 import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
-import ConfirmModal from '../components/ConfirmModal';
+import HomePage from './HomePage/HomePage.js'
 
 class App extends Component {
 
@@ -44,7 +44,6 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
@@ -52,6 +51,8 @@ class App extends Component {
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={HomePage} />
+
                             </Switch>
                         </span>
 
