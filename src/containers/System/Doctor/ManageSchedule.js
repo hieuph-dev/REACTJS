d11fe7a209c2f05daa8cf9b87763652e5e18532f
Hieu Pham <hieupham250201@gmail.com>
2022-11-04 14:57:04 +0700

    upload

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -9,6 +9,7 @@ import DatePicker from '../../../components/Input/DatePicker';
 import moment from 'moment';
 import { toast } from "react-toastify";
 import _ from 'lodash';
+import {saveBulkScheduleDoctor} from '../../../services/userService'
 
 class ManageSchedule extends Component {
     constructor(props) {
@@ -95,7 +96,7 @@ class ManageSchedule extends Component {
             })
         }
     }
-    handleSaveSchedule = () => {
+    handleSaveSchedule = async () => {
         let { rangeTime, selectedDoctor, currentDate } = this.state;
         let result = [];
         if (!currentDate) {
@@ -107,7 +108,9 @@ class ManageSchedule extends Component {
             return;
         }
         
-        let formatedDate = moment(currentDate).format(dateFormat.SEND_TO_SERVER);
+        // let formatedDate = moment(currentDate).format(dateFormat.SEND_TO_SERVER);
+
+        let formatedDate = new Date(currentDate).getTime();
 
         if(rangeTime && rangeTime.length > 0) {
             let selectedTime = rangeTime.filter(item => item.isSelected === true);
@@ -116,7 +119,7 @@ class ManageSchedule extends Component {
                     let object = {};
                     object.doctorId = selectedDoctor.value;
                     object.date = formatedDate;
-                    object.time = schedule.keyMap;
+                    object.timeType = schedule.keyMap;
                     result.push(object);
                 })
             } else {
@@ -125,6 +128,14 @@ class ManageSchedule extends Component {
             }
             console.log('selectedTime: ', selectedTime)
         }
+
+        let res = await saveBulkScheduleDoctor({
+            arrSchedule : result,
+            doctorId: selectedDoctor.value,
+            formatedDate: formatedDate
+        });
+        console.log('check res saveBulkSchedule ', res)
+
         console.log('check result: ', result)
     }
     render() {
--- src/services/userService.js
+++ src/services/userService.js
@@ -45,10 +45,14 @@ const getDetailInforDoctor = (inputId) => {
     return axios.get(`/api/get-detail-doctor-by-id?id=${inputId}`)
 }
 
+const saveBulkScheduleDoctor = (data) => {
+    return axios.post('/api/bulk-create-schedule', data)
+}
+
 export { 
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUserService, 
     editUserService, getAllCodeService, getTopDoctorHomeService , 
     getAllDoctors,saveDetailDoctorService,
-    getDetailInforDoctor
+    getDetailInforDoctor, saveBulkScheduleDoctor
 }
